ca6c414da8c0109ee3fed571ea8fb96f6cb7ffec
Alberto Casagrande <alberto.casagrande@gmail.com>
2019-04-13 18:05:46 +0200

    Minor update of the tests files

---
+++ tests/__init__.py
@@ -0,0 +1,22 @@
+"""A simple Python Model Checking package
+
+.. moduleauthor:: Alberto Casagrande <acasagrande@units.it>
+
+"""
+
+__author__ = "Alberto Casagrande"
+__copyright__ = "Copyright 2015-2019"
+__credits__ = ["Alberto Casagrande"]
+__license__ = "GPL"
+__release__ = "1.0"
+__subrelease__ = "1"
+__version__ = __release__+"."+__subrelease__
+__maintainer__ = "Alberto Casagrande"
+__email__ = "acasagrande@units.it"
+__status__ = "Development"
+
+
+name = "pyModelChecking"
+
+from .kripke import *
+
